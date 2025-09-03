357a45e50be6650bda9cc5150f6e81d7708661e9
Shivam Gravity <shivamgravity@gmail.com>
2025-09-03 14:24:57 +0530

    updated cli.py

---
+++ mytool001/cli.py
@@ -0,0 +1,7 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="My first CLI tool")
+    parser.add_argument("name", help="Your name > ")
+    args = parser.parse_args()
+    print(f"Hello, {args.name}!")
