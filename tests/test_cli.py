fadcf586b85fedac58f47c2271e193c53d4eae38
Shivam Gravity <shivamgravity@gmail.com>
2025-09-03 15:02:47 +0530

    updated test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,5 @@
 import subprocess
 
 def test_cli():
-    result = subprocess.run(["mytool001", "World"], capture_output=True, text=True)
-    assert "Hello, World!" in result.stdout
+    result = subprocess.run(["mytool001", "John", "Morning"], capture_output=True, text=True)
+    assert "Hello John! Good Morning." in result.stdout
